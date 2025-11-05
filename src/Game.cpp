a5a5aaf8cedc0fba323ff9c577b278cbef5de405
josh <Josh2.Cheetham@live.uwe.ac.uk>
2025-11-05 23:29:43 +0000

    Finalised visual polishing, fixed overlapping stamps and background texture

--- src/Game.cpp
+++ src/Game.cpp
@@ -24,7 +24,7 @@ bool Game::init()
 	font.loadFromFile("../Data/Fonts/OpenSans-Bold.ttf");
 
 	menu_text.setString("Welcome to Critters Crossing! Press the Play button to start, or the Quit button to exit.");
-	menu_text.setFillColor(sf::Color(255, 255, 0));
+	menu_text.setFillColor(sf::Color(150, 75, 0));
 	menu_text.setFont(font);
 	menu_text.setCharacterSize(25);
 	menu_text.setPosition(
@@ -32,7 +32,7 @@ bool Game::init()
 		window.getSize().y / 4 - menu_text.getGlobalBounds().height / 2);
 
 	play_text.setString("Play");
-	play_text.setFillColor(sf::Color(255, 255, 0));
+	play_text.setFillColor(sf::Color(150, 75, 0));
 	play_text.setFont(font);
 	play_text.setCharacterSize(30);
 	play_text.setPosition(
@@ -40,7 +40,7 @@ bool Game::init()
 		window.getSize().y / 2 - play_text.getGlobalBounds().height / 2);
 
 	quit_text.setString("Quit");
-	quit_text.setFillColor(sf::Color(255, 255, 0));
+	quit_text.setFillColor(sf::Color(150, 75, 0));
 	quit_text.setFont(font);
 	quit_text.setCharacterSize(30);
 	quit_text.setPosition(
@@ -50,14 +50,14 @@ bool Game::init()
 	lives = 3;
 	livesText.setFont(font);
 	livesText.setCharacterSize(30);
-	livesText.setFillColor(sf::Color::White);
+	livesText.setFillColor(sf::Color(150, 75, 0));
 	livesText.setPosition(10, 10);
 	livesText.setString("Lives: " + std::to_string(lives));
 
 	gameOver_text.setFont(font);
 	gameOver_text.setString("Game Over");
 	gameOver_text.setCharacterSize(60);
-	gameOver_text.setFillColor(sf::Color::Red);
+	gameOver_text.setFillColor(sf::Color(150, 75, 0));
 	gameOver_text.setPosition(
 		window.getSize().x / 2 - gameOver_text.getGlobalBounds().width / 2, 50);
 
@@ -68,6 +68,10 @@ bool Game::init()
 	}
 
 	background.setTexture(background_texture);
+	background.setScale(
+		window.getSize().x / background.getLocalBounds().width,
+		window.getSize().y / background.getLocalBounds().height
+	);
 
 	character = new sf::Sprite();
 	passport = new sf::Sprite();
@@ -80,33 +84,33 @@ bool Game::init()
 		return false;
 	}
 	sf::Texture* passport_texture_1 = new sf::Texture();
-	if (!passport_texture_1->loadFromFile("../Data/Images/penguin passport.png"))
+	if (!passport_texture_1->loadFromFile("../Data/Images/kenney_physicspack/PNG/Other/penguin passport.png"))
 	{
 		std::cout << "Failed to load penguin passport.png" << std::endl;
 		return false;
 	}
 
 	sf::Texture* animal_texture_2 = new sf::Texture();
-	if (!animal_texture_2->loadFromFile("../Data/Images/elephant.png"))
+	if (!animal_texture_2->loadFromFile("../Data/Images/kenney_physicspack/PNG/Other//elephant.png"))
 	{
 		std::cout << "Failed to load elephant.png" << std::endl;
 		return false;
 	}
 	sf::Texture* passport_texture_2 = new sf::Texture();
-	if (!passport_texture_2->loadFromFile("../Data/Images/Critter Crossing Customs/elephant passport.png"))
+	if (!passport_texture_2->loadFromFile("../Data/Images/kenney_physicspack/PNG/Other/elephant passport.png"))
 	{
 		std::cout << "Failed to load elephant passport.png" << std::endl;
 		return false;
 	}
 
 	sf::Texture* animal_texture_3 = new sf::Texture();
-	if (!animal_texture_3->loadFromFile("../Data/Images/Critter Crossing Customs/giraffe.png"))
+	if (!animal_texture_3->loadFromFile("../Data/Images/kenney_physicspack/PNG/Other/giraffe.png"))
 	{
 		std::cout << "Failed to load giraffe.png" << std::endl;
 		return false;
 	}
 	sf::Texture* passport_texture_3 = new sf::Texture();
-	if (!passport_texture_3->loadFromFile("../Data/Images/Critter Crossing Customs/moose passport.png"))
+	if (!passport_texture_3->loadFromFile("../Data/Images/kenney_physicspack/PNG/Other/moose passport.png"))
 	{
 		std::cout << "Failed to load moose passport.png" << std::endl;
 		return false;
@@ -253,25 +257,30 @@ void Game::mouseClicked(sf::Event event)
 				dragged_sprite = passport;
 				drag_offset.x = dragged_sprite->getPosition().x - click_position_f.x;
 				drag_offset.y = dragged_sprite->getPosition().y - click_position_f.y;
+
+				if (accept_button->getGlobalBounds().contains(click_position_f))
+				{
+					passport_stamped = true;
+					passport_accepted = true;
+					accept_button->setPosition(-1000, -1000);
+					reject_button->setPosition(-1000, -1000);
+				}
+				else if (reject_button->getGlobalBounds().contains(click_position_f))
+				{
+					passport_stamped = true;
+					passport_accepted = false;
+					accept_button->setPosition(-1000, -1000);
+					reject_button->setPosition(-1000, -1000);
+				}
 			}
 		}
-		if (accept_button->getGlobalBounds().contains(click_position_f))
+		else if (event.mouseButton.button == sf::Mouse::Right)
 		{
-			passport_stamped = true;
-			passport_accepted = true;
-		}
-		else if (reject_button->getGlobalBounds().contains(click_position_f))
-		{
-			passport_stamped = true;
-			passport_accepted = false;
-		}
-	}
-	else if (event.mouseButton.button == sf::Mouse::Right)
-	{
-		if (!passport_stamped)
-		{
-			accept_button->setPosition(click_position_f.x, click_position_f.y);
-			reject_button->setPosition(click_position_f.x, click_position_f.y + 50);
+			if (!passport_stamped)
+			{
+				accept_button->setPosition(click_position_f.x, click_position_f.y);
+				reject_button->setPosition(click_position_f.x, click_position_f.y + 150);
+			}
 		}
 	}
 }
